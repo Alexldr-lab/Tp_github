628e03f7b60fb81e37e10d170ef19c06ace6a5e5
Alexandre_L <alexandre.landrein@esme.fr>
2025-11-10 12:17:24 +0100

    première collaboration Github

---
+++ main.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 10 10:58:40 2025
+
+@author: alexa
+"""
+
+import pandas as pd 
+from sklearn import svm  
+from train_model import train_model
+from preprocess_data import preprocess_data
+
+iris = pd.read_csv(r"C:\Users\alexa\TP_Git\InPutData\Iris.csv") #load the dataset
+test_size = 0.3 # the attribute test_size=0.3 to use for splitting the data 
+				#into 70% for train and 30% for test
+
+train, test =preprocess_data(iris, test_size)
+# training data features
+train_X = train[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]
+# target of our training data
+train_y=train.Species
+# test data features
+test_X= test[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]
+#target value of test data
+test_y =test.Species   
+
+model = svm.SVC()
+prediction = train_model(train_X, train_y, test_X, model)
+
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
+
+acc = accuracy_score(test_y, prediction)
+print(f"Accuracy sur le test set : {acc:.2f}")
+
+ConfusionMatrixDisplay.from_predictions(test_y, prediction)
+import matplotlib.pyplot as plt
+plt.show()
---
+++ train_model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 10 11:00:16 2025
+
+@author: alexa
+"""
+
+def train_model(train_X, train_y, test_X, model):
+    """entraine un modele ML de classification et retourne des predictions sur
+    le dataset de test"""
+    model.fit(train_X,train_y) 
+    prediction=model.predict(test_X)
+    return prediction
+
+
